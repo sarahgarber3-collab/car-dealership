9033acb15330b9fff3fabaaac04483235ba25202
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-05-08 11:15:40 -0400

    Dealership file manager updates

--- src/main/java/DealershipFileManager.java
+++ src/main/java/DealershipFileManager.java
@@ -1,7 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DealershipFileManager {
 
     public static Dealership getDealership() {
-            return null;
+        Dealership dealership = new Dealership("Super Cool Car Imports", "111 Old Benbrook Rd", "817-555-5555");
+
+        try {
+            BufferedReader buffReader = new BufferedReader(new FileReader("src/main/resources/inventory.csv"));
+
+            String vehicleData;
+            buffReader.readLine(); //deals with header row
+            while ((vehicleData = buffReader.readLine()) != null) {
+                String[] splitVehicles = vehicleData.split("\\|");
+
+                int vin = Integer.parseInt(splitVehicles[0]);
+                int year = Integer.parseInt(splitVehicles[1]);
+                String make = splitVehicles[2];
+                String model = splitVehicles[3];
+                String vehicleType = splitVehicles[4];
+                String color = splitVehicles[5];
+                int odometer = Integer.parseInt(splitVehicles[6]);
+                double price = Double.parseDouble(splitVehicles[7]);
+
+                Vehicle vehicle = new Vehicle(vin, year, odometer, make, model, vehicleType, color, price);
+                dealership.addVehicle(vehicle);
+            }
+
+            buffReader.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return dealership;
     }
 
     public static void saveDealership(Dealership dealership) {
