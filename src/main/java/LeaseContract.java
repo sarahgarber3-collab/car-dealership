1b47329c0b47e4625cc97571e35cfb88ba4c584a
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-05-15 12:16:20 -0400

    Methods for lease contract are updated

--- src/main/java/LeaseContract.java
+++ src/main/java/LeaseContract.java
@@ -4,10 +4,19 @@ public class LeaseContract extends Contract {
     }
     @Override
     public double getTotalPrice(){
+        double price = vehicleSold.getPrice();
+        double expectedEndingValue = price * 0.50;
+        double leaseFee = price * 0.07;
 
+        return expectedEndingValue + leaseFee + price;
     }
     @Override
     public double getMonthlyPayment(){
+        double totalPrice = getTotalPrice();
+        double rate = 0.04 / 12;
+        int months = 36;
+
+        return(totalPrice * rate) / (1 - Math.pow(1 + rate, -months));
 
     }
 }
